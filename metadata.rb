d641219963cf0fed099f921fd334908a74198fd4
shimizukawa <shimizukawa@gmail.com>
2014-12-05 19:29:22 +0900

    add name metadata field

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "mercurial-env"
 maintainer       "Takayuki SHIMIZUKAWA"
 maintainer_email "shimizukawa@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures mercurial-env"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.4"
+version          "0.2.5"
